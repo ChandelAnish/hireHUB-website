ecadd4444012dc30a703cc6d188a6e1951437ac6
ChandelAnish <anish8427singh@gmail.com>
2024-09-19 23:11:09 +0530

    minor changes

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://hirehub-3n9d.onrender.com";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://hirehub-3n9d.onrender.com";
