d98451c92524c9e494f6db50ae32c7615e174e13
Arindam <17arindambera@gmail.com>
2024-10-03 01:39:01 +0530

    fix:js logic shift to script.js

--- client/login-page/script.js
+++ client/login-page/script.js
@@ -75,3 +75,16 @@ const emailVerification = async () => {
     const userdetails = await getUserProfile(username);
     sessionStorage.setItem('userdetails', JSON.stringify(userdetails));
 }
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+ 
+  const type = password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+
+  
+  this.classList.toggle("fa-eye-slash");
+  this.classList.toggle("fa-eye");
+});
