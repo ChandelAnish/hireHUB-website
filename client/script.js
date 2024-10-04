0f87c69914c4626bb34ed68513170f341cfbab41
shreyad2806 <dubeyshreya2507@gmail.com>
2024-10-04 14:37:13 +0530

    Added back to top button

--- client/script.js
+++ client/script.js
@@ -2,3 +2,13 @@ let btn=document.getElementById("nextbtn")
 setInterval(()=>{
     btn.click()
 },2500)
+
+// Get the Back to Top button and the top anchor
+const backToTopButton = document.getElementById('back-to-top');
+const topAnchor = document.getElementById('top');
+
+// Add an event listener to the Back to Top button
+backToTopButton.addEventListener('click', () => {
+  // Scroll to the top of the page
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
